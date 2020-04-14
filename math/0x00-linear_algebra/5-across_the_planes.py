2d636919e8defed8ca32695fb1544283870ca11c
karenahv <you@example.com>
2020-04-13 19:33:18 -0500

    number 5

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""add two 2d matrix"""
+
+
+def add_matrices2D(mat1, mat2):
+    if (len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0])):
+        result = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+                  for i in range(len(mat1))]
+        return result
+    else:
+        return None
