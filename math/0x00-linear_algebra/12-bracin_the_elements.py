3c37f34824c759d0600a58d4e8ef2ccbc900c57f
karenahv <you@example.com>
2020-04-14 18:44:32 -0500

    number 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""add, substraction, multiplication, division matrix """
+
+
+import numpy as np
+
+
+def np_elementwise(mat1, mat2):
+    return (np.add(mat1, mat2), np.subtract(mat1, mat2),
+            np.multiply(mat1, mat2), np.divide(mat1, mat2))
