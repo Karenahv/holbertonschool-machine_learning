6626081cd3952f08b5e5f223a65d5a256873f807
karenahv <you@example.com>
2020-04-13 19:41:48 -0500

    number 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    result = []
+    for item in arr1:
+        result.append(item)
+    for item in arr2:
+        result.append(item)
+    return result
