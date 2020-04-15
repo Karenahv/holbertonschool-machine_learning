69f82d7756452fb1d2480b10b99591fdd231923e
karenahv <you@example.com>
2020-04-15 07:37:09 -0500

    correction style

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -6,5 +6,6 @@ import numpy as np
 
 
 def np_elementwise(mat1, mat2):
+    """add, substraction, mult, div"""
     return (np.add(mat1, mat2), np.subtract(mat1, mat2),
             np.multiply(mat1, mat2), np.divide(mat1, mat2))
