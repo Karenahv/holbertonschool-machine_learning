46377a5406d5de73e02f20dc827ab06963865d88
karenahv <you@example.com>
2020-04-15 07:26:49 -0500

    correction style 5 and 6

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -3,6 +3,7 @@
 
 
 def add_matrices2D(mat1, mat2):
+    """ """
     if (len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0])):
         result = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
                   for i in range(len(mat1))]
--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -3,6 +3,7 @@
 
 
 def cat_arrays(arr1, arr2):
+    """ """
     result = []
     for item in arr1:
         result.append(item)
