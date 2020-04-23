426ca5aebccbdd153622c1f6a7d0469d333c54de
karenahv <you@example.com>
2020-04-23 14:03:15 -0500

    number 10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -5,10 +5,13 @@
 def poly_derivative(poly):
     """poli derivate"""
     arr = []
-    if poly is None:
+    if poly is None or len(poly) == 0:
         return None
     if(len(poly) == 1):
         return [0]
-    for i in range(1, len(poly)):
-        arr.append(i*poly[i])
-    return arr
+    if type(poly) is list:
+        for i in range(1, len(poly)):
+            arr.append(i*poly[i])
+        return arr
+    else:
+        return None
