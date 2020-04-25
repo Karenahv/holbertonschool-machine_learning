9d3591e31957a59d555fb2a9e7e0d731b1542dbe
karenahv <you@example.com>
2020-04-25 11:43:01 -0500

    number 17

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-"""derivate polinomial"""
+"""poli integral"""
 
 
 def poly_integral(poly, C=0):
-    """poli derivate"""
-    arr = [0]
+    """poli integral"""
+    arr = [C]
     arr2 = [0]
     if type(poly) != list:
         return None
     if poly is None or len(poly) == 0 or poly == []:
         return None
-    if(len(poly) == 1):
-        return [0]
+    if(type(C) is not int):
+        return None
+    if(poly == [0]):
+        return [C]
     for elem in poly:
         if(type(elem) is not int and type(elem) is not float):
             return None
@@ -19,7 +21,11 @@ def poly_integral(poly, C=0):
         for item in poly:
             arr2.append(item)
         for i in range(1, len(arr2)):
-            arr.append(arr2[i]/i)
+            aux = arr2[i]/i
+            if (aux).is_integer():
+                arr.append(int(arr2[i]/i))
+            else:
+                arr.append(arr2[i]/i)
         return arr
     else:
         return None
