58961c03ae8eedf4123bc573e9a6f9e36ae91af7
karenahv <you@example.com>
2020-04-29 13:05:03 -0500

    comments

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -6,40 +6,47 @@ e = 2.7182818285
 
 
 class Poisson():
-    """ class Poisson"""
+    """ Class to calculate Poisson distribution"""
+
     def __init__(self, data=None, lambtha=1.):
-        """constructor"""
+        """Constructor of Poisson
+        """
         self.lambtha = float(lambtha)
         if data is None:
-            if lambtha < 0:
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
         else:
             if isinstance(data, list):
-                if (len(data) > 1):
+                if len(data) > 1:
                     self.data = data
-                    self.lambtha = float(sum(self.data)/len(self.data))
+                    self.lambtha = float(sum(self.data) / len(self.data))
                 else:
                     raise ValueError("data must contain multiple values")
             else:
                 raise TypeError("data must be a list")
 
     def pmf(self, k):
-        """ calculates pmf"""
-        k = int(k)
-        if k < 0:
-            return (0)
-        dnominador = 1
-        for i in range(1, k + 1):
-            dnominador = dnominador * i
-        pmf = (e ** -self.lambtha * self.lambtha ** k)/dnominador
-        return pmf
+        """ Calculates the Probability Mass Function of the distribution.
+        """
+        x = int(k)
+        if x < 0:
+            return 0
+        else:
+            x_fact = 1
+            for i in range(1, x + 1):
+                x_fact = x_fact * i
+
+            f = e ** -self.lambtha * self.lambtha ** x / x_fact
+            return f
 
     def cdf(self, k):
-        """cdf"""
-        if k < 0:
-            return (0)
+        """ Calculates the cumulative distribution function
+        """
         k = int(k)
+        if k < 0:
+            return 0
+
         cdf = 0
         for i in range(k + 1):
-            cdf = cdf + self.pmf(i)
+            cdf += self.pmf(i)
         return cdf
