49422a23d043a0e8e13eec63877cfce21c40d4f8
karenahv <you@example.com>
2020-05-03 15:03:59 -0500

    getters class Neuron

---
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" single neuron performing
+    binary classification
+"""
+
+import numpy as np
+
+
+class Neuron():
+    """ class neuron bainary
+    classification"""
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        w = np.random.randn(nx)
+        self.__W = np.array([w])
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter private bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter private output neuron A"""
+        return self.__A
