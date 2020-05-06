3296ec609b7300be7fd0c5828e1824bccd0e7d49
karenahv <you@example.com>
2020-05-06 13:46:14 -0500

    number 9

--- supervised_learning/0x00-binary_classification/9-neural_network.py
+++ supervised_learning/0x00-binary_classification/9-neural_network.py
@@ -1,69 +1,61 @@
 #!/usr/bin/env python3
-""" single neuron performing
-    binary classification
-"""
-
+"""Neural Network"""
 import numpy as np
 
 
 class NeuralNetwork():
-    """ class neural Network"""
+    """Defines a neural network"""
+
     def __init__(self, nx, nodes):
-        if not isinstance(nx, int):
+        """ Class constructor..
+        """
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(nodes, int):
+        if type(nodes) is not int:
             raise TypeError("nodes must be an integer")
-        if nx < 1:
-            raise ValueError("nodes must be a positive integer")
-        w1 = [np.random.randn(nx).tolist() for i in range(nodes)]
-        self.__W1 = np.array(w1)
-        self.__b1 = [np.zeros(1).tolist() for i in range(nodes)]
+        if nodes < 1:
+            raise TypeError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes * nx).reshape(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
-        w2 = np.random.randn(nodes)
-        self.__W2 = np.array([w2])
+        self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
 
     @property
     def W1(self):
-        """
-        return Weights first layer
+        """ W1 attribute getter.
         """
         return self.__W1
 
     @property
-    def W2(self):
-        """
-        return Weights hidden layer
-        """
-        return self.__W2
-
-    @property
     def b1(self):
-        """
-        return bias first layer
+        """ b1 attribute getter.
         """
         return self.__b1
 
     @property
-    def b2(self):
-        """
-        return bias hidden layer
-        """
-        return self.__b2
-
-    @property
     def A1(self):
-        """
-        return Active hidden layer 1
+        """ A1 attribute getter.
         """
         return self.__A1
 
     @property
-    def A2(self):
+    def W2(self):
+        """ W2 attribute getter.
         """
-        return Active output 2
+        return self.__W2
+
+    @property
+    def b2(self):
+        """ b2 attribute getter
+        """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """ A2 attribute getter.
         """
         return self.__A2
