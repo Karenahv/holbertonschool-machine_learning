8b0226cd4eee425e61f8e375af126dad159d554f
karenahv <you@example.com>
2020-05-06 13:36:05 -0500

    8

--- supervised_learning/0x00-binary_classification/8-neural_network.py
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
-""" single neuron performing
-    binary classification
-"""
-
+"""Neural Network"""
 import numpy as np
 
 
 class NeuralNetwork():
-    """ class neural Network"""
+    """Defines a neural network"""
+
     def __init__(self, nx, nodes):
-        if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+        """ Class constructor.
+        """
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        if not isinstance(nodes, int):
-            raise TypeError("nodes must be an integer")
-        if nx < 1:
-            raise ValueError("nodes must be a positive integer")
-        w1 = [np.random.randn(nx).tolist() for i in range(nodes)]
-        self.W1 = np.array(w1)
-        self.b1 = [np.zeros(1).tolist() for i in range(nodes)]
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) != int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.W1 = np.random.randn(nodes * nx).reshape(nodes, nx)
+        self.b1 = np.zeros(nodes).reshape(nodes, 1)
         self.A1 = 0
-        w2 = np.random.randn(nodes)
-        self.W2 = np.array([w2])
+        self.W2 = np.random.randn(nodes).reshape(1, nodes)
         self.b2 = 0
         self.A2 = 0
