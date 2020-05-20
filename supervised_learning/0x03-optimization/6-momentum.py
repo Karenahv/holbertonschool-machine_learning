eff2684a6eae417f07d140755ffaff9113cf83c3
karenahv <you@example.com>
2020-05-19 20:30:22 -0500

    Number 6

---
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" train with momentum"""
+
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """train with momentum"""
+    return tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
+    
