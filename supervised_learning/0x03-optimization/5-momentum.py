aec62e236984fbd63db53eecc9e876a31a2487f6
karenahv <you@example.com>
2020-05-22 07:39:01 -0500

    number 5

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """gradient descent with momentum"""
 
-import tensorflow as tf
-
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """gradient descent with momentum """
