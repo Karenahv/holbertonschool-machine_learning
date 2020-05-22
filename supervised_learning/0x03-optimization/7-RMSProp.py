618ced3e3e2e52c16e1ff0259ffdd2c5f6d187d5
karenahv <you@example.com>
2020-05-22 07:43:25 -0500

    number7

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env pytho3
-""" RMSProp optimization algorithm"""
+#!/usr/bin/env python3
+"""
+RMSprop Optimization Algorithm
+"""
 
 
-def update_variables_RMSProp(alpha,
-                             beta2, epsilon,
-                             var, grad, s):
-    """Update a variable using RMSProp"""
-    Stw = (beta2 * s) + ((1 - beta2) * (grad ** 2))
-    var = var - ((alpha * grad) / ((Stw ** (1/2)) + epsilon))
-    return var, Stw
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    Returns: the updated variable and the new moment, respectively
+    """
+    Sd = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    var = var - ((alpha * grad) / ((Sd ** (1/2)) + epsilon))
+    return (var, Sd)
