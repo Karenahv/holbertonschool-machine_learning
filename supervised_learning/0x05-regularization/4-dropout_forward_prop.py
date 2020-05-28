c3f5302af1bd998ee4456ce37c8b06486fad8d8d
karenahv <you@example.com>
2020-05-28 07:21:05 -0500

    [fix] 4 drop

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
-"""forward propagation using dropout"""
-
+"""
+Forward Propagation with Dropout
+"""
 import numpy as np
 
 
+def softmax(z):
+    """takes as input a vector of K real numbers, and
+    normalizes it into a probability distribution"""
+    t = np.exp(z)
+    a = np.exp(z) / np.sum(t, axis=0, keepdims=True)
+    return a
+
+
 def dropout_forward_prop(X, weights, L, keep_prob):
-    """forward propagation using dropout"""
+    """
+    conducts forward propagation using Dropout
+    """
     cache = {}
     cache["A0"] = X
-    for i in range(1, L + 1):
-        keyw = "W{}".format(i)
-        keyb = "b{}".format(i)
-        keyA = "A{}".format(i - 1)
-
-        z = np.matmul(weights[keyw], cache[keyA])
-        z = z + weights[keyb]
-        if i != L:
-            drop = np.random.binomial(1, keep_prob, size=z.shape) / keep_prob
-            A = drop * np.tanh(z)
-            cache["D{}".format(i)] = drop
+    for lay in range(L):
+        Al = cache["A" + str(lay)]
+        Wl = weights["W" + str(lay + 1)]
+        bl = weights["b" + str(lay + 1)]
+        Zl = np.matmul(Wl, Al) + bl
+        if lay != L - 1:
+            a = np.sinh(Zl) / np.cosh(Zl)
+            drop = np.random.binomial(1, keep_prob, (a.shape[0], a.shape[1]))
+            cache["D" + str(lay + 1)] = drop
+            a = np.multiply(a, drop)
+            cache["A" + str(lay + 1)] = a/keep_prob
         else:
-            t = np.exp(z)
-            A = t / np.sum(t, axis=0, keepdims=True)
-
-        cache["A" + str(i)] = A
+            cache["A" + str(lay + 1)] = softmax(Zl)
     return cache
