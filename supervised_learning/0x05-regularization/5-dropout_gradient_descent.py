ccf6caaf71b7996c21b9bad54033970a71e68e0c
karenahv <you@example.com>
2020-05-28 13:58:03 -0500

    fix number 5

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""Gradient descent with Dropout"""
+""" Gradient Descent with Dropout"""
 
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """GD with dropout regularization"""
-    m = Y.shape[1]
-    dz = 0
     temp_weights = weights.copy()
-
+    dz = 0
+    m = Y.shape[1]
     for i in range(L, 0, -1):
         keyw = "W{}".format(i)
         keyb = "b{}".format(i)
@@ -18,15 +17,14 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
 
         if i == L:
             dz = (cache[keyout] - Y)
-            dw = np.matmul(dz, cache[X].transpose()) / m
+            dw = np.matmul(dz, cache[X].T) / m
         else:
-            d1 = 1 - (cache[keyout]) ** 2
+            d1 = 1 - ((cache[keyout]) * (cache[keyout]))
             weight = temp_weights["W{}".format(i + 1)]
-            dropout = cache["D{}".format(i)] * d1
-            dz_layer = np.matmul(weight.T, dz) * dropout
-            dw = (1/m) * np.matmul(dz_layer, cache[X].T)
-            dz = dz_layer
-
-        db = np.sum(dz, axis=1, keepdims=True) / m
-        weights[keyw] = temp_weights[keyw] - (alpha * dw)
+            drop = (cache["D{}".format(i)] / keep_prob) * d1
+            dz_l = np.matmul(weight.T, dz) * drop
+            dw = (np.matmul(dz_l, cache[X].T)) / m
+            dz = dz_l
+        db = (dz.sum(axis=1, keepdims=True)) / m
+        weights[keyw] = temp_weights[keyw] - (alpha * (dw))
         weights[keyb] = temp_weights[keyb] - (alpha * db)
