19a8d118314aeda624ef85822a663444da128c5a
karenahv <you@example.com>
2020-05-28 07:11:18 -0500

    [fix] number3

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -6,9 +6,9 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """creates tensorflow layer with l2 regularization"""
-    reg_weights = tf.contrib.layers.l2_regularizer(lambtha)
     initialize = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(units=n, activation=activation,
+    reg_weights = tf.contrib.layers.l2_regularizer(lambtha)
+    layer = tf.layers.Dense(name='layer', units=n, activation=activation,
                             kernel_initializer=initialize,
                             kernel_regularizer=reg_weights)
     return layer(prev)
