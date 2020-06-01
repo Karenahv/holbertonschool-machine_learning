ba2d1dc0b4cda1e1352ff4b27f45970c156e5f3c
karenahv <you@example.com>
2020-06-01 13:51:56 -0500

    change loss

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -7,6 +7,6 @@ import tensorflow.keras as K
 def optimize_model(network, alpha, beta1, beta2):
     """ Optimization of a Keras Model"""
     network.compile(optimizer=K.optimizers.Adam(alpha, beta1, beta2),
-                    loss=K.losses.CategoricalCrossentropy(from_logits=True),
+                    loss='categorical_crossentropy',
                     metrics=['accuracy'])
     return None
