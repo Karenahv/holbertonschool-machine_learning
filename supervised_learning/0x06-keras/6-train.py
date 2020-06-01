bef46f2270f11fce428545f210e3bf404a4e8ea9
karenahv <you@example.com>
2020-06-01 17:56:20 -0500

    Number 6

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" train a model using keras"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """ train a model using keras"""
+    callback = None
+    if early_stopping and validation_data:
+        callback = [K.callbacks.EarlyStopping(patience=patience)]
+    history = network.fit(data, labels, epochs=epochs, batch_size=batch_size,
+                          shuffle=shuffle, validation_data=validation_data,
+                          callbacks=callback)
+    return history
