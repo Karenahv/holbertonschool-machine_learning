cce81ca5b0efc8f22b9157d6ae6010a044e88252
karenahv <you@example.com>
2020-06-04 13:34:25 -0500

    number 2 fix shape

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
-"""performs a same convolution on grayscale images"""
-
+""" Same Convolution """
 import numpy as np
 
 
 def convolve_grayscale_padding(images, kernel, padding):
-    """""performs a same convolution on grayscale images"""
-    m = len(images)
-    h = len(images[0])
-    w = len(images[1])
-    hk = len(kernel)
-    wk = len(kernel[0])
-    h_final = (h - hk) + 1
-    w_final = (w - wk) + 1
+    """ performs a same convolution on grayscale images:
+    Returns: a numpy.ndarray containing the convolved images
+    """
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
     ph = padding[0]
     pw = padding[1]
     new_img = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
                      mode='constant', constant_values=0)
-    h_final = h_final + 2 * ph
-    w_final = w_final + 2 * pw
+    h_new = new_img.shape[1]
+    w_new = new_img.shape[2]
+    h_final = (h_new - kh) + 1
+    w_final = (w_new - kw) + 1
     array = np.zeros((m, h_final, w_final))
     img = np.arange(m)
-    for i in range(int(h_final)):
-        for j in range(int(w_final)):
-            array[img, i, j] = (np.sum(new_img[img, i:hk + i, j:wk + j] *
+    for j in range(h_final):
+        for i in range(w_final):
+            array[img, j, i] = (np.sum(new_img[img, j:kh+j, i:kw+i] *
                                        kernel, axis=(1, 2)))
     return array
