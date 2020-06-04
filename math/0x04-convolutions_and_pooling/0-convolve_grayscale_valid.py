d48cf4b352e31d96e8520230359e6b16493df351
karenahv <you@example.com>
2020-06-04 12:30:36 -0500

    correction shape

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-"""performs a valid convolution on grayscale images"""
-
+""" Valid Convolution """
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
-    """""performs a valid convolution on grayscale images"""
+    """ performs a valid convolution on grayscale images:
+    Returns: a numpy.ndarray containing the convolved images
+    """
     m = images.shape[0]
     h = images.shape[1]
-    w = images.shape[1]
-    hk = kernel.shape[0]
-    wk = kernel.shape[1]
-    h_final = (h - hk) + 1
-    w_final = (w - wk) + 1
-    array = np.zeros((m, h_final, w_final))
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    conv = np.zeros((m, h-kh+1, w-kw+1))
     img = np.arange(m)
-    for i in range(h_final):
-        for j in range(w_final):
-            array[img, i, j] = (np.sum(images[img, i:hk + i, j:wk + j] *
-                                       kernel, axis=(1, 2)))
-    return array
+    for j in range(h-kh+1):
+        for i in range(w-kw+1):
+            conv[img, j, i] = (np.sum(images[img, j:kh+j, i:kw+i] *
+                               kernel, axis=(1, 2)))
+    return conv
