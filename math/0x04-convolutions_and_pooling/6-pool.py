be6ace58be115fcb967147316cd0f1b7010fdb14
karenahv <you@example.com>
2020-06-04 21:50:38 -0500

    correction w_final

--- math/0x04-convolutions_and_pooling/6-pool.py
+++ math/0x04-convolutions_and_pooling/6-pool.py
@@ -16,7 +16,7 @@ def pool(images, kernel_shape, stride, mode='max'):
     sh = stride[0]
     sw = stride[1]
     h_final = int(((h-kh)/sh) + 1)
-    w_final = int(((h-kw)/sw) + 1)
+    w_final = int(((w-kw)/sw) + 1)
     array = np.zeros((m, h_final, w_final, c))
     img = np.arange(m)
     for j in range(h_final):
