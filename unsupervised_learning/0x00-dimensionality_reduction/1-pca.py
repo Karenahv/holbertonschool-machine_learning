fb83a3e92db865893eeb5d77bafc18a02deec278
Karenahv <837@holbertonschool.com>
2020-07-29 14:07:55 -0500

    change linalg to svd

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -6,8 +6,8 @@ import numpy as np
 
 def pca(X, ndim):
     """Performs PCA on dataset"""
-    (n, d) = X.shape
-    X = X - np.tile(np.mean(X, 0), (n, 1))
-    (l, M) = np.linalg.eig(np.dot(X.T, X))
-    Y = np.dot(X, M[:, 0:ndim])
-    return Y
+    X_m = X - np.mean(X, axis=0)
+    u, s, vh = np.linalg.svd(X_m)
+    W = vh[:ndim].T
+    T = np.matmul(X_m, W)
+    return T
