d6e2b022858071e61c1f69dcbad1d36d4ef21f2d
Karenahv <837@holbertonschool.com>
2020-07-29 20:02:01 -0500

    Q affinities

--- unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -1,29 +1,26 @@
-# !/usr/bin/env python3
-"""
-5. Q affinities
-"""
+#!/usr/bin/env python3
+""" t-SNE (Stochastic Neighbor Embedding)"""
 
 import numpy as np
 
 
 def Q_affinities(Y):
-    """ Function that calculates the Q affinities
-    Args:
-        Y is a numpy.ndarray of shape (n, ndim) containing the low
-            dimensional transformation of X
-    Returns: Q, num
-        Q - is a numpy.ndarray of shape (n, n) containing the Q affinities
-        num - is a numpy.ndarray of shape (n, n) containing the numerator
-            of the Q affinities
-    Documentation:
-        https://lvdmaaten.github.io/tsne/
     """
-    n, ndim = Y.shape
-    # (a-b)^2 = a^2 + b^2 - 2*a*b
-    Y_square = np.sum(np.square(Y), axis=1)
-    XY = np.dot(Y, Y.T)
-    D = np.add(np.add((-2 * XY), Y_square).T, Y_square)
-
+    * Y is a numpy.ndarray of shape (n, ndim) containing the
+      low dimensional transformation of X
+      - n is the number of points
+    * ndim is the new dimensional representation of X
+    Returns: Q, num
+    * Q is a numpy.ndarray of shape (n, n) containing
+      the Q affinities
+    * num is a numpy.ndarray of shape (n, n) containing
+      the numerator of the Q affinities
+    """
+    n, d = Y.shape
+    x_square = np.sum(np.square(Y), axis=1)
+    y_square = np.sum(np.square(Y), axis=1)
+    xy = np.dot(Y, Y.T)
+    D = np.add(np.add((-2 * xy), x_square).T, y_square)
     Q = np.zeros((n, n))
     num = np.zeros((n, n))
     for i in range(n):
