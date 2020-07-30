fe174c5b827c74cf3e64841fbde29319cdd044b3
Karenahv <837@holbertonschool.com>
2020-07-29 20:32:51 -0500

    cost of transformation

--- unsupervised_learning/0x00-dimensionality_reduction/7-cost.py
+++ unsupervised_learning/0x00-dimensionality_reduction/7-cost.py
@@ -1,15 +1,19 @@
 # !/usr/bin/env python3
-""" Cost """
+"""Cost function"""
+
 import numpy as np
 
 
 def cost(P, Q):
-    """ calculates the cost of the t-SNE transformation
-    - P is a numpy.ndarray of shape (n, n) containing the P affinities
-    - Q is a numpy.ndarray of shape (n, n) containing the Q affinities
+    """
+    Cost function
+    Args:
+        P: numpy.ndarray of shape (n, n) containing the P affinities
+        Q: numpy.ndarray of shape (n, n) containing the Q affinities
     Returns: C, the cost of the transformation
     """
-    Q = np.where(Q != 0, Q, 1e-12)
-    P = np.where(P != 0, P, 1e-12)
-    cost = np.sum(P * np.log(P / Q))
-    return cost
+    # C = Smi,Smj (Pj|i log(Pj|i / Qj|i))
+    P = np.maximum(P, 1e-12)
+    Q = np.maximum(Q, 1e-12)
+    C = np.sum(P * np.log(P / Q))
+    return C
