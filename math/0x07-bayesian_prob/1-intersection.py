5e963762339563de943d89d425f4aeb2bada8308
Karenahv <837@holbertonschool.com>
2020-07-31 20:09:04 -0500

    intersection

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python3
-"""intersection of obtaining this data
-with the various hypothetical probabilities"""
-
+""" Intersection """
 import numpy as np
 
 
 def intersection(x, n, P, Pr):
-    """
-    :param x:is the number of patients that develop severe side effects
-    :param n: is the total number of patients observed
-    :param P: is a 1D numpy.ndarray containing the various
-    hypothetical probabilities of developing severe side effects
-    :param Pr: is a 1D numpy.ndarray containing the prior beliefs of P
-    :return: a 1D numpy.ndarray containing
-    the intersection of obtaining x and n with each
-    probability in P, respectively
+    """ calculates the intersection of obtaining this data with the various
+        hypothetical probabilities
     """
     if not isinstance(n, int) or n < 1:
         raise ValueError("n must be a positive integer")
@@ -28,13 +19,14 @@ def intersection(x, n, P, Pr):
     if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.amin(P) < 0 or np.amax(P) > 1:
-        raise ValueError("All values in P must be   in the range [0, 1]")
+        raise ValueError("All values in P must be in the range [0, 1]")
     if np.amin(Pr) < 0 or np.amax(Pr) > 1:
         raise ValueError("All values in Pr must be in the range [0, 1]")
     if not np.isclose([np.sum(Pr)], [1])[0]:
         raise ValueError("Pr must sum to 1")
-    # f = (n! / (x! * (n-x)!)) * (p ** x) * (1-p) ** (n-x)
+
     fact = np.math.factorial
-    first_part = fact(n) / (fact(x) * fact(n - x))
-    likelihood = first_part * (P ** x) * ((1 - P) ** (n - x))
+    tmp = fact(n) / (fact(x) * fact(n - x))
+    likelihood = tmp * (P**x) * ((1 - P)**(n - x))
     return likelihood * Pr
+
