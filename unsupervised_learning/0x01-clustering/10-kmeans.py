3555237c1181145d7eb48b763522848d0d216983
Karenahv <837@holbertonschool.com>
2020-08-05 19:41:22 -0500

    numner 10

---
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""perform K-means with
+    sklearn.cluster"""
+
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """
+    X is a numpy.ndarray of shape (n, d) containing the dataset
+    k is the number of clusters
+    The only import you are allowed to
+     use is import sklearn.cluster
+    Returns: C, clss
+    C is a numpy.ndarray of shape (k, d)
+     containing the centroid means for each cluster
+    clss is a numpy.ndarray of shape (n,) containing
+     the index of the cluster in C that each data point belongs to
+    """
+    kmeans = sklearn.cluster.KMeans(n_clusters=k, random_state=0).fit(X)
+    return kmeans.cluster_centers_, kmeans.labels_
