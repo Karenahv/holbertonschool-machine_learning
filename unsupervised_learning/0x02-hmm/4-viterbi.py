f9b20886480dbc8655d9c62c5b33dead4837134e
Karenahv <837@holbertonschool.com>
2020-08-13 09:11:41 -0500

    number 4

--- unsupervised_learning/0x02-hmm/4-viterbi.py
+++ unsupervised_learning/0x02-hmm/4-viterbi.py
@@ -53,19 +53,35 @@ def viterbi(Observation, Emission, Transition, Initial):
         return None, None
     if not np.sum(Initial) == 1:
         return None, None
+    y = Observation
+    A = Transition
+    B = Emission
+    Pi = Initial
 
-    V = np.zeros((N, T))
-    B = np.zeros((N, T))
-    V[:, 0] = Initial.T * Emission[:, Observation[0]]
-    # for each t, for each state, choose the
-    # biggest from all prev-state * transition * ob,
-    # remember the best prev
-    for t in range(1,  T):
-        for j in range(N):
-            V[j, t], B[j, t] = max([(p, s)
-                                    for s, p in enumerate(V[:, t - 1]
-                                                          * Transition[:, j]
-                                                          * Emission[j, Observation[t]])])
-    P = np.amax(V, axis=0)
+    # Cardinality of the state space
+    K = A.shape[0]
+    # Initialize the priors with default (uniform dist) if not given by caller
+    Pi = Pi if Pi is not None else np.full(K, 1 / K)
+    T = len(y)
+    T1 = np.empty((K, T), 'd')
+    T2 = np.empty((K, T), 'B')
+
+    # Initilaize the tracking tables from first observation
+    T1[:, 0] = Pi.T * B[:, y[0]]
+    T2[:, 0] = 0
+
+    # Iterate throught the observations updating the tracking tables
+    for i in range(1, T):
+        T1[:, i] = np.max(T1[:, i - 1] * A.T * B[np.newaxis, :, y[i]].T, 1)
+        T2[:, i] = np.argmax(T1[:, i - 1] * A.T, 1)
+
+    # Build the output, optimal model trajectory
+    x = np.empty(T, 'B')
+    x[-1] = np.argmax(T1[:, T - 1])
+    for i in reversed(range(1, T)):
+        x[i - 1] = T2[x[i], i]
+
+    P = np.amax(T1[:, y - 1], axis=0)
     P = np.amin(P)
-    return V, P
+
+    return x, P
